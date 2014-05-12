67a4d52db2c72abfabde5af6a8bda77318a8a26e
Nick Krichevsky <njk828@gmail.com>
2014-05-12 08:23:32 -0400

    add color incrementer to python

--- py/main.py
+++ py/main.py
@@ -1,49 +1,67 @@
 import random
 
+def letterIncrement(letter):
+	incrementNextValuePlace=False
+	try:
+		c=int(letter)
+		c+=1
+		if c>9:
+			return ('A',incrementNextValuePlace)
+		else:
+			return (c,incrementNextValuePlace)
+	except ValueError:
+		c=None
+		if letter != 'F':
+			if letter=='A':
+				c='B'
+			elif letter=='B':
+				c='C'
+			elif letter=='C':
+				c='D'
+			elif letter=='D':
+				c='E'
+			elif letter=='E':
+				c='F'
+		elif letter == 'F':
+			c=0
+			incrementNextValuePlace=True
+		if c is not None:
+			return (c,incrementNextValuePlace)
 def incrementHex(hexValue):
 	if type(hexValue)!=str:
 		raise ValueError
 	hexValue=hexValue.upper()
+	if hexValue[0]=='#':
+		hexValue=hexValue[1:]
+	if len(hexValue)!=6:
+		raise ValueError("Invalid Color Code")
 	if hexValue=='FFFFFF':
 		raise ValueError("Maxiumum Color code value")
-	reversedValue=''.join(reversed(hexValue)) #Reverse the hex value
-	tempValue=""
-	for char,i in zip(reversedValue,range(len(reversedValue))):
-		try:
-			c=int(char)
-			c+=1
-			if c<10:
-				c=str(c)
-			else:
-				c='A'
-			tempValue+=c+reversedValue[(i+1):]
-			break
-			
-		except ValueError:
-			if char != 'F':
-				if char=='A':
-					c='B'
-				elif char=='B':
-					c='C'
-				elif char=='C':
-					c='D'
-				elif char=='D':
-					c='E'
-				elif char=='E':
-					c='F'
-				tempValue+=c+reversedValue[(i+1):]
-				break
-			elif char=='F':
-				c=0
-				tempValue+=str(c)
-	finalValue=''.join(reversed(tempValue))
-	return finalValue
+	rgb=[hexValue[i:i+2] for i in range (0,len(hexValue),2)] #Split the color code every two chars, for [R,G,B]
+	newRgb=[]
+	for value in rgb:
+		if len(value)!=2:
+			raise ValueError("Invalid color code")
+		char=value[1]
+		fc=value[0] #First character
+		c=None #Char we check first
+		resultC,increment=letterIncrement(char)
+		c=str(resultC)
+		if (increment):
+			result2,increment2=letterIncrement(fc)
+			fc=str(result2)
+		newRgb.append(fc+c)
+	newValue=""
+	for item in newRgb:
+		newValue+=item
+	return newValue
+		
 
 choices="0123456789ABCDEF"
 hexValue=""
 for i in range(6):
 	hexValue+=random.choice(choices)
+print hexValue
 for i in range (10000):
 	hexValue=incrementHex(hexValue)
 	print hexValue
-			
