7656d74a645446b145129b85f2bfe1c6a876bd83
Nick Krichevsky <njk828@gmail.com>
2014-05-12 09:19:52 -0400

    Write web portion, with random generation

--- web/main.js
+++ web/main.js
@@ -1,70 +1,119 @@
-function reverseString(string){
-	var rev=""
-	for (var i=string.length-1; i>=0; i--){
-		rev+=string[i]
-	}
-	return rev
-}
+var rInc=false
+var gInc=false
+var bInc=false
 
-function hexIncrement(hexValue){
-	hexValue=hexValue.toUpperCase()
-	if (hexValue=='FFFFFF'){
-		return 'FFFFFF'
+function letterIncrement(letter){
+	incrementNeeded=false
+	var parsedChar=parseInt(letter)
+	if (!isNaN(parsedChar)){
+		parsedChar++
+		if (parsedChar>9){
+			parsedChar="A"
+		}
+		return {'char':parsedChar,'increment':incrementNeeded}
 	}
-	var reversedValue=reverseString(hexValue)
-	var tempValue=""
-	for (var i=0; i<reversedValue.length; i++){
-		var char = reversedValue[i]
-		var parsedChar=parseInt(char)
-		if (!isNaN(parsedChar)){
-			parsedChar++
-			if (parsedChar>9){
-				parsedChar="A"
+	else{
+		if (letter!='F'){
+			if (letter=='A'){
+				letter='B'
 			}
-			tempValue+=parsedChar
-			for (var j=i+1; j<reversedValue.length; j++){
-				tempValue+=reversedValue[j]
+			else if (letter=='B'){
+				letter='C'
 			}
-			break;
+			else if (letter=='C'){
+				letter='D'
+			}
+			else if (letter=='D'){
+				letter='E'
+			}
+			else if (letter=='E'){
+				letter='F'
+			}
+			return {'char':letter,'increment':incrementNeeded}
+		}
+		else if (letter=='F'){
+			letter=0
+			incrementNeeded=true
+			return {'char':letter,'increment':incrementNeeded}
+		}
+
+	}
+}
+function hexIncrement(hexValue){
+	if (hexValue[0]=='#'){
+		var newValue=""
+		for (var i=1; i<hexValue.length; i++){
+			newValue+=hexValue[i]
+		}
+		hexValue=newValue
+	}
+	if (hexValue.length!=6){
+		return undefined
+	}
+	var rgb=""
+	splitValues=hexValue.match(/.{1,2}/g)
+	for (var i=0; i<splitValues.length; i++){
+		
+		if (splitValues[i].length!=2){
+			return undefined
+		}
+		var inc;
+		var firstChar=splitValues[i][0]
+		var char=splitValues[i][1]
+		if ((i==0 && !rInc) || (i==1 && !gInc) || (i==2 && !bInc)){
+			var result=letterIncrement(char)
+			char = result['char']
+			inc = result['increment']
 		}
 		else{
-			if (char!='F'){
-				if (char=='A'){
-					char='B'
-				}
-				else if (char=='B'){
-					char='C'
-				}
-				else if (char=='C'){
-					char='D'
-				}
-				else if (char=='D'){
-					char='E'
-				}
-				else if (char=='E'){
+			char='F'
+		}
+		if (inc==true){
+			var result2=letterIncrement(firstChar)
+			var prevFirstChar=firstChar
+			firstChar=result2['char']
+			if (prevFirstChar=='F' && firstChar==0){
+				firstChar='F'
+				if (i==0){
+					rInc=true
 					char='F'
 				}
-				tempValue+=char
-				for (var j=i+1; j<reversedValue.length; j++){
-					tempValue+=reversedValue[j]
+				else if (i==1){
+					gInc=true
+					char='F'
 				}
-				break;
-			}
-			else if (char=='F'){
-				char=0
-				tempValue+=char
+				else if (i==2){
+					bInc=true
+					char='F'
+				}
 			}
 		}
+		rgb+=firstChar
+		rgb+=char
 	}
-	var finalValue=reverseString(tempValue)
-	return finalValue
+	return rgb
+	
+};
+var v = ""
+var choices = "123456789ABCDEF"
+for (var i=0; i<6; i++){
+	v+=choices[Math.floor(Math.random()*16)]
 }
-var v="F8301E"
+prev=""
 $(document).ready(function(){
-	for (var i=0; i<1000; i++){
+	for (var i=0; i<256; i++){
 		v=hexIncrement(v)
-		$('body').append("<span style=\" display:inline-block; height:100px; width:auto; background-color:"+v+";\">YOOYOY</span><br />")
+		if (prev=="FFFFFF" && v=="FFFFFF"){
+			break;
+		}
+		$('body').append("<div style=\" display:flex; height:50px; width:auto; background-color:"+v+";\"></div>")
 		console.log("<span \"style=height:100px; width:auto; color:"+v+";\"/>")
+		if (v==undefined){
+			console.log("")
+		}
+		
+		prev=v
 		console.log(v)
 	}	
+	$('body').append("POT OF GOLD")
 });
